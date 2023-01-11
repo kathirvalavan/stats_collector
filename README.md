5979b5d664b92a8cb052b293a9d23e263deaf6c5
kathir <kathirvalan.ict@gmail.com>
2023-01-11 13:02:40 +0530

    added stats

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # stats_collector
+
+https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/Statistics-definitions.html
--- lib/stats_collector.rb
+++ lib/stats_collector.rb
@@ -21,6 +21,13 @@ class StatsCollector
   def report(report_for: '', data: nil)
     @data_set_config[report_for].collect(data: data)
   end
+
+  def generate_stats
+    @data_set_config.each do |_, v|
+      v.generate_stats
+    end
+  end
+
 end
 
 class StatsDataSet
@@ -39,4 +46,55 @@ class StatsDataSet
   def collect(data: nil)
     @data_set << data
   end
+
+  def generate_stats
+    stats = StatsGenerator.new(data_set: @data_set)
+    stats.sample_count
+  end
 end
+
+# https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/Statistics-definitions.html
+# inspired from this
+
+class StatsGenerator
+
+  def initialize(data_set: )
+    @data_set = data_set
+  end
+
+  def sample_count
+  end
+
+  def sum
+  end
+
+  def average
+  end
+
+  def minimum
+  end
+
+  def maximum
+  end
+
+  def percentile
+  end
+
+  def trimmed_percentile
+  end
+
+  def winsorized_mean
+  end
+
+  def percentile_rank
+  end
+
+  def trimmed_count
+  end
+
+  def trimmed_sum
+  end
+
+end
+
+
